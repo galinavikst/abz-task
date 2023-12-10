231871c0c915032075958651c2b611250aeebaa5
Halyna Stepanenko <galinavikst@gmail.com>
2023-12-10 15:31:56 +0100

    fix: habdle image error, userName css - nowrap

--- app/components/card/Card.tsx
+++ app/components/card/Card.tsx
@@ -1,5 +1,6 @@
 import style from "./card.module.scss";
 import Tooltip from "@mui/material/Tooltip";
+import defaultUser from "../../../public/default-user.svg";
 
 type UserData = {
   photo: string;
@@ -14,10 +15,22 @@ type UserProps = {
 };
 
 export default function Card({ user }: UserProps) {
+  const handleImageError = (
+    e: React.SyntheticEvent<HTMLImageElement, Event>
+  ) => {
+    const target = e.target as HTMLImageElement;
+    target.src = defaultUser.src;
+    target.onerror = null;
+  };
   return (
     <li className={style.card}>
       <div>
-        <img loading="lazy" src={user.photo} alt="user photo" />
+        <img
+          loading="lazy"
+          src={user.photo || defaultUser.src}
+          alt="user photo"
+          onError={handleImageError}
+        />
       </div>
 
       <p>{user.name}</p>
